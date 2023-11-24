03ea03db74c067c18afb2373713999590bc9e895
Dist Bit <adp01@pm.me>
2023-11-24 14:59:09 +1100

    printPowerConsumption.sh timestamp.txt

--- printPowerConsumption.sh
+++ printPowerConsumption.sh
@@ -1,1 +1,10 @@
-echo - | awk "{printf \"%.1f\", $(( $(cat /sys/class/power_supply/BATT/current_now) * $(cat /sys/class/power_supply/BATT/voltage_now) )) / 1000000000000 }"; echo " W"
+#!/bin/bash
+
+ac_status=$(cat /sys/class/power_supply/ACAD/online)
+echo $ac_status
+power_consumption=$(echo "scale=1; $(cat /sys/class/power_supply/BATT/current_now) * $(cat /sys/class/power_supply/BATT/voltage_now) / 1000000000000" | bc)
+if [ "$ac_status" -eq 0 ]; then
+    power_consumption="-$power_consumption"
+fi
+
+echo "$power_consumption W"
