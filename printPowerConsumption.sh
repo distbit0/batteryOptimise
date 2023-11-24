365a404d33fe8706d81553218e1b69f5838d4828
Dist Bit <adp01@pm.me>
2023-11-24 15:00:13 +1100

    printPowerConsumption.sh

--- printPowerConsumption.sh
+++ printPowerConsumption.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ac_status=$(cat /sys/class/power_supply/ACAD/online)
-echo $ac_status
 power_consumption=$(echo "scale=1; $(cat /sys/class/power_supply/BATT/current_now) * $(cat /sys/class/power_supply/BATT/voltage_now) / 1000000000000" | bc)
 if [ "$ac_status" -eq 0 ]; then
     power_consumption="-$power_consumption"
+fi
+if [ "$ac_status" -eq 1 ]; then
+    power_consumption="+$power_consumption"
 fi
 
 echo "$power_consumption W"
