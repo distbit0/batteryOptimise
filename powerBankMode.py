f2fc8f66dd8ae21efd5e38e30bb84832a8c00685
Dist Bit <adp01@pm.me>
2023-12-05 14:10:05 +1100

    powerBankMode.py printPowerConsumption.py printPowerConsumption.sh timestamp.txt

--- powerBankMode.py
+++ powerBankMode.py
@@ -6,35 +6,6 @@ import tkinter as tk
 from tkinter import font as tkFont
 
 
-def show_notification_for_one_second(message):
-    root = tk.Tk()
-    # Set the window title
-    root.title("Notification")
-
-    # Create a label with the provided text
-    large_font = tkFont.Font(family="Helvetica", size=30)
-
-    # Create a label with the provided text using the large font
-    label = tk.Label(root, text=message, font=large_font)
-    label.pack()
-
-    # Update the window size and position
-    root.update_idletasks()  # Update the window's internal data
-    window_width = root.winfo_width()
-    window_height = root.winfo_height()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    x = int((screen_width - window_width) / 2)
-    y = int((screen_height - window_height) / 2)
-    root.geometry(f"+{x}+{y}")  # Set the window position
-
-    # Schedule the window to close after 1000 milliseconds (1 second)
-    root.after(1000, root.destroy)
-
-    # Run the main loop to display the window
-    root.mainloop()
-
-
 def getAbsPath(relPath):
     basepath = path.dirname(__file__)
     fullPath = path.abspath(path.join(basepath, relPath))
---
+++ printPowerConsumption.py
@@ -0,0 +1,36 @@
+def read_file(path):
+    with open(path, "r") as file:
+        return file.read().strip()
+
+
+# Read AC status, current (in microamperes), and voltage (in microvolts)
+ac_status = read_file("/sys/class/power_supply/ACAD/online")
+current_now = float(read_file("/sys/class/power_supply/BATT/current_now"))
+voltage_now = float(read_file("/sys/class/power_supply/BATT/voltage_now"))
+
+# Calculate power consumption in watts
+power_consumption = current_now * voltage_now / 10**12
+
+# Adjust sign based on AC status
+if ac_status == "0":
+    power_consumption = -power_consumption
+elif ac_status == "1":
+    power_consumption = +power_consumption
+
+# Read battery's current charge and total capacity (in microamperes-hours)
+current_charge = float(read_file("/sys/class/power_supply/BATT/charge_now"))
+total_capacity = float(read_file("/sys/class/power_supply/BATT/charge_full"))
+
+if power_consumption != 0:
+    if power_consumption > 0:
+        time_remaining = (total_capacity - current_charge) / current_now
+    else:
+        time_remaining = current_charge / current_now
+else:
+    time_remaining = float("inf")  # Represents an infinite amount of time
+
+# Round both values to one decimal place
+power_consumption_rounded = round(power_consumption, 1)
+time_remaining_rounded = round(time_remaining, 1)
+
+print(f"{power_consumption_rounded} W | {time_remaining_rounded} H")
